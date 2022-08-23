a4d914c13e59a4e3a6b7e8ef6c6b6f0615b0f882
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-23 14:44:28 -0500

    Add the info into the DOM using javascript

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import './style.css';
 
-const test = 0;
-console.log(test);
+const arrayTest = [{
+  name: 'Federico',
+  score: 1,
+  id: 0
+},{
+  name: 'Luis',
+  score: 2,
+  id: 1
+},{
+  name: 'Ivan',
+  score: 3,
+  id: 2
+},
+{
+  name: 'Mauricio',
+  score: 4,
+  id: 2
+}]
+
+document.getElementById('scoreTable').innerHTML = arrayTest.map((items) => 
+  `<li>${items.name}: ${items.score}</li>`
+).join('');
