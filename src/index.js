cb178a5bc9c5bd83a5b87b1406524f53c33f92ec
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-23 14:55:34 -0500

    Fix the linters details

--- src/index.js
+++ src/index.js
@@ -3,22 +3,20 @@ import './style.css';
 const arrayTest = [{
   name: 'Federico',
   score: 1,
-  id: 0
-},{
+  id: 0,
+}, {
   name: 'Luis',
   score: 2,
-  id: 1
-},{
+  id: 1,
+}, {
   name: 'Ivan',
   score: 3,
-  id: 2
+  id: 2,
 },
 {
   name: 'Mauricio',
   score: 4,
-  id: 2
-}]
+  id: 2,
+}];
 
-document.getElementById('scoreTable').innerHTML = arrayTest.map((items) => 
-  `<li>${items.name}: ${items.score}</li>`
-).join('');
+document.getElementById('scoreTable').innerHTML = arrayTest.map((items) => `<li>${items.name}: ${items.score}</li>`).join('');
