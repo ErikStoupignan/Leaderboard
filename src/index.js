2acc72008be05921b766fc1ec04484a623ae1bc3
Erik Stoupignan Sánchez Beltrán <eriksanchezbeltran@outlook.com>
2022-08-25 15:13:28 -0500

    Adding async function in the display app and styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import genBoard from './modules/Gen-Board.js';
 import addElement from './modules/addElement.js';
 import getBoard from './modules/Get-Board.js';
 
-const id = 'QgHcOIXPyYQqqLJkTuJF';
+// const id = 'QgHcOIXPyYQqqLJkTuJF';
+const id = 'QgHcOIXPyYQqqLJkTuJ1'; //false Id
 const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
 
 const submitBtn = document.getElementById('input-button');
@@ -13,17 +14,17 @@ const inputName = document.getElementById('input-name');
 const updateBtn = document.getElementById('btnUpdate');
 
 // eslint-disable-next-line
-submitBtn.addEventListener('click', () => {
+submitBtn.addEventListener('click', async () => {
   if (inputScore.value === '' || inputName.value === '') {
     return;
   }
 
   const name = inputName.value;
   const score = inputScore.value;
-  inputName.value = ''; inputScore.value = '';
+  // inputName.value = ''; inputScore.value = '';
 
-  addElement(name, score, id, url);
-  getBoard(genBoard, id, url);
+  await addElement(name, score, id, url);
+  await getBoard(genBoard, id, url);
 });
 
 updateBtn.addEventListener('click', () => {
